bf296cae4fb574a54da1902e70ae66020ac6ac8d
kxnaylorCLE216 <naylorkx@gmail.com>
2019-06-10 20:11:34 -0400

    Added some tests in the 5th part of the tutorial

--- tutorial/polls/models.py
+++ tutorial/polls/models.py
@@ -7,6 +7,9 @@ from django.utils import timezone
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
+    def was_published_recently(self):
+        now = timezone.now()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
     def __str__(self):
         return self.question_text
 
@@ -17,5 +20,3 @@ class Choice(models.Model):
     votes = models.IntegerField(default=0)
     def __str__(self):
         return self.choice_text
-        def was_published_recently(self):
-            return self.pub_date >= timezone.now() - datetime.timedelta(days=1)
